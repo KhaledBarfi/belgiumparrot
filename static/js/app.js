1094d3cf6a1369cd6a7ddd3d6cc521244b4ae28e
Khaled Barfi <barfi.khaled@hotmail.com>
2025-08-19 13:05:40 +0200

    Front: app.js + inclusion (19/08/2025 13:05:40)

---
+++ static/js/app.js
@@ -0,0 +1,5 @@
+﻿// app.js - 19/08/2025
+document.addEventListener('DOMContentLoaded',()=>{
+  const b = document.querySelector('#bp-banner');
+  if(b){ setTimeout(()=> b.classList.add('show'), 150); }
+});
